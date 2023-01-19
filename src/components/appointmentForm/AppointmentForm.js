1b441805952370a0dad4f336ed19393d7ce0fac2
MiyanoX <62541265+MiyanoX@users.noreply.github.com>
2023-01-19 23:11:47 +0800

    Step 8

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -12,6 +13,7 @@ export const AppointmentForm = ({
   setTime,
   handleSubmit
 }) => {
+
   const getTodayString = () => {
     const [month, day, year] = new Date()
       .toLocaleDateString("en-US")
@@ -19,7 +21,29 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  const handleTitle = (e) => {
+    return setTitle(e.target.value);
+  }
+
+  const handleContact = (e) => {
+    return setContact(e.target.value);
+  }
+
+  const handleDate = (e) => {
+    return setDate(e.target.value);
+  }
+
+  const handleTime = (e) => {
+    return setTime(e.target.value);
+  }
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit} >
+      <input type='text' value={title} placeholder="Title" onChange={handleTitle} ></input>
+      <input type='date' value={date} placeholder="Date" onChange={handleDate} min={getTodayString()} ></input>
+      <input type='time' value={time} placeholder="Time" onChange={handleTime} ></input>
+      <ContactPicker value={contact} contacts={contacts} onChange={handleContact} ></ContactPicker>
+      <input type="submit" value="submit" ></input>
+    </form>
   );
 };
--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const ContactPicker = () => {
   return (
-    ContactPicker
+    <p ></p>
   );
 };
